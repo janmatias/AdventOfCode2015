6a3bb1a1884cffdba88f969820e682161c065ae7
janmatias <jan.orstavik@gmail.com>
2015-12-06 16:03:21 +0100

    Completed day 3. Added error specific to day 3 in json.

---
+++ day_03/functions.py
@@ -0,0 +1,44 @@
+class infiniteGrid():
+	def __init__(self):
+		self.position = (0,0)
+		self.robotPosition = (0,0)
+		self.receivedGift = {self.position : 1}
+
+	def updatePositionAndGive(self, newPosition, robot):
+		if (newPosition in self.receivedGift.keys()):
+			self.receivedGift[newPosition] += 1
+		else:
+			self.receivedGift[newPosition] = 1
+
+		if (robot):
+			self.robotPosition = newPosition
+		else:
+			self.position = newPosition
+# ---
+def resolve(lines, part):
+	
+	ig = infiniteGrid()
+	iteration = 0
+	for c in lines[0]:
+		if (part == '1' or iteration == 0):
+			intPos = ig.position
+			robot = False
+		elif(part == '2' and iteration == 1):
+			intPos = ig.robotPosition
+			robot = True
+		else:
+			throw(100)
+
+		if (c == '^'):
+			ig.updatePositionAndGive((intPos[0], intPos[1]+1), robot)
+		elif (c == 'v'):
+			ig.updatePositionAndGive((intPos[0], intPos[1]-1), robot)
+		elif (c == '<'):
+			ig.updatePositionAndGive((intPos[0]-1, intPos[1]), robot)
+		elif (c == '>'):
+			ig.updatePositionAndGive((intPos[0]+1, intPos[1]), robot)
+		else:
+			throw(10)
+		iteration = (iteration+1)%2
+
+	return len(ig.receivedGift)
