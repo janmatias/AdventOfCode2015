345d05be2c4b1929c7b8512334c17585c67d5730
janmatias <jan.orstavik@gmail.com>
2015-12-08 10:52:20 +0100

    Initial setup complete. ,(Undefined location of -f argument is trivial, but time consuming)

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sys
+import sys, os.path
 from errorHandling.handler import *
+
+def checkForDataFileCommandLineArgument():
+	if ('-f' in sys.argv and sys.argv.index('-f') != len(sys.argv)-1):
+		dataFilePath = sys.argv[sys.argv.index('-f') + 1]
+	else:
+		dataFilePath = None
+		throw(2)
 
 # Options
 day = '1'
@@ -19,20 +26,32 @@ elif (len(sys.argv) == 3):
 		part = sys.argv[2]
 	else:
 		throw(1)
+elif (len(sys.argv) == 5):
+	checkForDataFileCommandLineArgument()
 else:
 	throw(2)
 
 # Import correct day
-l = ["day_0"+str(x) for x in range(1, 10)]
-l.extend(["day_"+str(x) for x in range(10, 25)])
+dayList = ["day_0"+str(x) for x in range(1, 10)]
+dayList.extend(["day_"+str(x) for x in range(10, 25)])
 
-package = l[int(day)-1]
+package = dayList[int(day)-1]
 module = __import__(package + '.functions', fromlist=["resolve"])
 resolve = getattr(module, "resolve")
 
+if (dataFilePath is None):
+	if (os.path.isfile(package + "/data.txt")):
+		dataFilePath = package + "/data.txt"
+	elif (os.path.isfile(package + "/input.txt")):
+		dataFilePath = package + "/input.txt"
+	else:
+		throw(11)
+elif (not os.path.isfile(dataFilePath)):
+	throw(11)
+
 # Load data
 lines = []
-data = open(package + "/data.txt", "r")
+data = open(dataFilePath, "r")
 for line in data:
 	line = line.replace("\n", "")
 	lines.append(line)
