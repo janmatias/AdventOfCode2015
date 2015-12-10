50591290e55c580911a415685d32eaace8a399ed
janmatias <jan.orstavik@gmail.com>
2015-12-10 20:07:56 +0100

    guessed structural setup.

--- day_11/functions.py
+++ day_11/functions.py
@@ -1,5 +1,16 @@
+v = False
 
+def doSomethingFancy(line):
+	pass
 
 # ---
-def resolve(lines, part):
-	return None
+def resolve(lines, part, verbose=False):
+	v = verbose
+
+	result = 0
+	for line in lines:
+		line = line.replace("\n", "")
+
+		result += doSomethingFancy(line)
+
+	return result
