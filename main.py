71cd59892a7b9ad69ef69327794b304e985e0411
janmatias <jan.orstavik@gmail.com>
2015-12-10 19:47:41 +0100

    Removed personal arg parser and implemented argparse module. Added a file not found error code.

--- main.py
+++ main.py
@@ -1,52 +1,33 @@
-import sys, os.path
+import sys, os.path, argparse
 from errorHandling.handler import *
 
-def checkForDataFileCommandLineArgument():
-	if ('-f' in sys.argv and sys.argv.index('-f') != len(sys.argv)-1):
-		dataFilePath = sys.argv[sys.argv.index('-f') + 1]
-	else:
-		dataFilePath = None
-		throw(2)
+parser = argparse.ArgumentParser(description="This program is able to run the adventofcode solutions.")
 
-# Options
-day = '1'
-part = '1'
-partsForDay = [['1', '2'] for x in range(24)]
-days = [str(x) for x in range(1,25)]
+parser.add_argument('day', type=int, choices=[x for x in range(1, 26)], help="Varible to define what day to run.")
+parser.add_argument('part', nargs='?', const='1', default='1', choices=['1', '2'], help="Variable to define what part of the challenge to solve.")
+parser.add_argument('-t', '--testing', action='store_true', help="Boolean to set the test data file as the input.")
 
-# Check command line arguments to initiate correct day
-if (len(sys.argv) == 2):
-	if (sys.argv[1] in days):
-		day = sys.argv[1]
-	else:
-		throw(1)
-elif (len(sys.argv) == 3):
-	if (sys.argv[1] in days and sys.argv[2] in partsForDay[int(sys.argv[1])-1]):
-		day = sys.argv[1]
-		part = sys.argv[2]
-	else:
-		throw(1)
-elif (len(sys.argv) == 5):
-	checkForDataFileCommandLineArgument()
-else:
-	throw(2)
+args = parser.parse_args()
 
 # Import correct day
 dayList = ["day_0"+str(x) for x in range(1, 10)]
 dayList.extend(["day_"+str(x) for x in range(10, 25)])
 
-package = dayList[int(day)-1]
+package = dayList[int(args.day)-1]
 module = __import__(package + '.functions', fromlist=["resolve"])
 resolve = getattr(module, "resolve")
 
-if (dataFilePath is None):
-	if (os.path.isfile(package + "/data.txt")):
-		dataFilePath = package + "/data.txt"
-	elif (os.path.isfile(package + "/input.txt")):
-		dataFilePath = package + "/input.txt"
-	else:
-		throw(11)
-elif (not os.path.isfile(dataFilePath)):
+dataFilePath = None
+# Find data file
+if (args.testing):
+	dataFilePath = package + "/test.txt"
+	if (not os.path.isfile(dataFilePath)):
+		throw(3)
+elif (os.path.isfile(package + "/data.txt")):
+	dataFilePath = package + "/data.txt"
+elif (os.path.isfile(package + "/input.txt")):
+	dataFilePath = package + "/input.txt"
+else:
 	throw(11)
 
 # Load data
@@ -58,5 +39,5 @@ for line in data:
 data.close()
 
 # Execute main program
-result = resolve(lines, part)
+result = resolve(lines, args.part)
 print ("Result: " + str(result))
