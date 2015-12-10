7fada97fdf813bf632c98509a5178a533a04dd06
janmatias <jan.orstavik@gmail.com>
2015-12-10 09:12:37 +0100

    day 10 completed

--- day_10/functions.py
+++ day_10/functions.py
@@ -1,5 +1,32 @@
-
+def lookAndSay(s):
+	cc = None # cc - current character
+	count = 1
+	newS = ""
+	for c in s:
+		if (c == cc):
+			count += 1
+		else:
+			if (cc is not None):
+				newS += str(count) + cc
+			cc = c
+			count = 1
+	if (cc is not None):
+		newS += str(count) + cc
+	return newS
 
 # ---
 def resolve(lines, part):
-	return None
+
+	if (part == '2'):
+		iterations = 50
+	else:
+		iterations = 40
+
+	result = ""
+	for line in lines:
+		for _ in range(iterations):
+			line = lookAndSay(line)
+		result += str(len(line)) + "\n"
+	return result
+
+
