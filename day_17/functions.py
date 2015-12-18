5e1a362d4207f4980751c3d201c322c42e22a7df
janmatias <jan.orstavik@gmail.com>
2015-12-18 16:56:07 +0100

    Completed Day 17

--- day_17/functions.py
+++ day_17/functions.py
@@ -1,5 +1,30 @@
-
+import itertools
 
 # ---
 def resolve(lines, part):
-	return None
+	eggnog = 150 # Amiunt of liquid to contain
+	containers = []
+
+	for line in lines:
+		containers.append(int(line))
+
+	possibilities = 0
+	minNumberOfContainers = len(containers)
+	for i in range(len(containers)):
+		minSum = 0
+		for comb in itertools.combinations(containers, i):
+			s = sum(comb)
+			minSum = min(s, minSum)
+			if (s == eggnog):
+				minNumberOfContainers = min(minNumberOfContainers, len(comb))
+				possibilities += 1
+		if (minSum > eggnog): # If sum minimum combination is greater than eggnog, there is no point in trying larger combinations
+			break
+
+	if (part == '2'):
+		possibilities = 0
+		for comb in itertools.combinations(containers, minNumberOfContainers):
+			if (sum(comb) == eggnog):
+				possibilities += 1
+
+	return possibilities
