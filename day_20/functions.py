493d583777a9c961eca1cd30b205eb82d3ec4435
janmatias <jan.orstavik@gmail.com>
2015-12-26 12:21:22 +0100

    Day 20 completed.

--- day_20/functions.py
+++ day_20/functions.py
@@ -1,68 +1,26 @@
-import math
+def deliverPresentsUpToHouses(limit, part):
+	houses = [0] * limit
 
-def getPrimes(n):
-	primes = []
-	for i in range(2, n):
-		for x in range(2, i):
-			if i % x == 0:
-				break
-		else:
-			primes.append(i)
-	return primes
+	for elf in range(1, limit):
+		delivered = 0
+		for elfDeliveries in range(elf, limit, elf): # deliver present for each elf elf
+			if (part == '1'):
+				houses[elfDeliveries] += elf*10
+			elif (delivered < 50):
+				houses[elfDeliveries] += elf*11
+				delivered += 1
+	return houses
 
-primes = [2]
-
-def getNumberOfPresentsForHouse(house):
-	num = 0
-	"""
-	steps = 1
-	for p in primes:
-		if (p >= house):
-			break
-		if (house % p != 0):
-			steps *= p
-	"""
-	for i in range(1, int(math.sqrt(house)+2)):
-		#print(i)
-		if (house % i == 0):
-			num += i*10
-	return num
-
-# ---
+#-- 
 def resolve(lines, part):
-	minPresents = int(lines[0])
-	print("Min presents: " + str(minPresents))
-	"""
-	value = 0
-	house = 0
-	while (value < minPresents):
-		house += 1
-		value = 0
-		for i in range(1, house+1):
-			value += i * 10
+	target = int(lines[0])
+	
+	houses = deliverPresentsUpToHouses(1000000, part)
+	
+	# Find first house to hit (or pass) target
+	for i in range(len(houses)):
+		if (houses[i] >= target):
+			return i
 
-	product = value
-	print(house)
-	for i in range(1, house+1):
-		print(i)
-		if (isPrime(i)):
-			product *= i
-	print(product)
-	return
-	print(getNumberOfPresentsForHouse(product))
-	return product
-
-	return house
-	"""
-
-
-	#print(getNumberOfPresentsForHouse(100000000))
-	value = 0
-	house = 100000
-	while (value < minPresents):# and house < 10):
-		house += 1
-		value = getNumberOfPresentsForHouse(house)
-		print(str(house) + ": " + str(minPresents - value))
-	return house
-
-
+	# If no house found, return illegal value (-1)
+	return -1
